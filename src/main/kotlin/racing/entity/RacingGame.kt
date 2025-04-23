7461d803d25961d5963f72b6ba2aa3d4a328e217
심홍섭 책임매니저 <simhongsup@hyundai.com>
2025-04-23 19:10:54 +0900

    style(game): 네이밍 정리

--- src/main/kotlin/racing/entity/RacingGame.kt
+++ src/main/kotlin/racing/entity/RacingGame.kt
@@ -10,11 +10,9 @@ class RacingGame(private val players: List<Player>, private val totalTryCount: I
 
     fun play() {
         while (currentTryCount < totalTryCount) {
-            val racingResults:ArrayList<PlayerRacingResult> = arrayListOf()
-            players.forEach {
-                racingResults.add(PlayerRacingResult(it, CarMovementDecider.isMovable()))
+            val racingResults = players.map { player ->
+                PlayerRacingResult(player, CarMovementDecider.isMovable())
             }
-
             histories.add(RacingHistoryPerCount(++currentTryCount, racingResults))
         }
     }
@@ -25,26 +23,36 @@ class RacingGame(private val players: List<Player>, private val totalTryCount: I
         val resultReduceMap = players.associateWith { 0 }.toMutableMap()
 
         for (history in histories.sortedBy { it.tryIndex }) {
-
             printlnWithTime("===== [${history.tryIndex}] 번째 결과 =======")
-            // 회차별 누적 결과 계산
-            history.racingResults.forEach {
-                if (it.isMove) {
-                    resultReduceMap[it.player] = resultReduceMap[it.player]!! + 1
-                }
-            }
+            // 이동 결과 누적결과 반영
+            updateMoveResults(history, resultReduceMap)
+            // 현재까지의 누적 결과 출력
+            printCurrentResults(resultReduceMap)
+        }
+        // 최종 우승자 출력
+        printFinalWinners(resultReduceMap)
+    }
 
-            // 회차별 누적 결과 출력
-            resultReduceMap.forEach { (key, value) ->
-                val text = "-".repeat(value)
-                printlnWithTime("${key.name} : $text")
+    private fun updateMoveResults(history: RacingHistoryPerCount, resultReduceMap: MutableMap<Player, Int>) {
+        history.racingResults.forEach {
+            if (it.isMove) {
+                resultReduceMap[it.player] = resultReduceMap[it.player]!! + 1
             }
         }
+    }
 
-        // 최종 우승자 구하기
+    private fun printCurrentResults(resultReduceMap: MutableMap<Player, Int>) {
+        resultReduceMap.forEach { (key, value) ->
+            val text = "-".repeat(value)
+            printlnWithTime("${key.name} : $text")
+        }
+    }
+
+    private fun printFinalWinners(resultReduceMap: MutableMap<Player, Int>) {
         val maxValue = resultReduceMap.values.maxOrNull()
         val winnerList = resultReduceMap.filter { it.value == maxValue }.keys.toList()
         winnerList.joinToString()
-        printlnWithTime("최종 우승자 : ${winnerList.joinToString { it.name }}")
+        printlnWithTime("최종 우승자 : ${winnerList.joinToString {it.name} }")
     }
+
 }
