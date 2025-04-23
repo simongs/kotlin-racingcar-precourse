b919a03dc967f6cdcf444b11c007982fe690db3c
심홍섭 책임매니저 <simhongsup@hyundai.com>
2025-04-23 18:58:02 +0900

    feature(game): 최종결과 출력

--- src/main/kotlin/racing/entity/RacingGame.kt
+++ src/main/kotlin/racing/entity/RacingGame.kt
@@ -22,23 +22,29 @@ class RacingGame(private val players: List<Player>, private val totalTryCount: I
     fun printWinPlayersInfo() {
         printlnWithTime("실행 결과")
 
-        val playerReduceMap = players.associateWith { 0 }.toMutableMap()
+        val resultReduceMap = players.associateWith { 0 }.toMutableMap()
 
         for (history in histories.sortedBy { it.tryIndex }) {
 
             printlnWithTime("===== [${history.tryIndex}] 번째 결과 =======")
+            // 회차별 누적 결과 계산
             history.racingResults.forEach {
                 if (it.isMove) {
-                    playerReduceMap[it.player] = playerReduceMap[it.player]!! + 1
+                    resultReduceMap[it.player] = resultReduceMap[it.player]!! + 1
                 }
             }
 
-            playerReduceMap.forEach { (key, value) ->
+            // 회차별 누적 결과 출력
+            resultReduceMap.forEach { (key, value) ->
                 val text = "-".repeat(value)
                 printlnWithTime("${key.name} : $text")
             }
         }
 
-        printlnWithTime("최종 우승자 : pobi, jun")
+        // 최종 우승자 구하기
+        val maxValue = resultReduceMap.values.maxOrNull()
+        val winnerList = resultReduceMap.filter { it.value == maxValue }.keys.toList()
+        winnerList.joinToString()
+        printlnWithTime("최종 우승자 : ${winnerList.joinToString { it.name }}")
     }
 }
